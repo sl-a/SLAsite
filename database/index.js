58f8710f1bc4a3b3e75477ad523f3a180058b784
Adam Reback <adamreback@Adams-MBP.attlocal.net>
2019-07-13 16:07:57 -0700

    Database | db connection established

--- database/index.js
+++ database/index.js
@@ -1,29 +1,24 @@
 const { Pool, Client } = require('pg');
 const config = require('../config.js');
+const connectionString = `postgresql://localhost:5432/sla`;
 
 const pool = new Pool({
-    user: config.postgresUser,
-    host: '3.16.25.245',
-    database: 'emails',
-    password: config.postgresPass,
-    port: 3211,
-  })
-  
-  pool.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    pool.end()
-  })
-  
-  const client = new Client({
-    user: 'dbuser',
-    host: 'database.server.com',
-    database: 'mydb',
-    password: 'secretpassword',
-    port: 3211,
-  })
-  client.connect()
-  
-  client.query('SELECT NOW()', (err, res) => {
-    console.log(err, res)
-    client.end()
-  })
+  connectionString: connectionString,
+})
+
+pool.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  pool.end()
+})
+
+const client = new Client({
+  connectionString: connectionString,
+})
+client.connect()
+
+client.query('SELECT NOW()', (err, res) => {
+  console.log(err, res)
+  client.end()
+})
+
+module.exports = client;
